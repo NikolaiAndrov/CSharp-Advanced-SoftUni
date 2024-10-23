b10d6ca8cd9f52e10da963ac5fafae2815cf8ea9
NikolaiAndrov <32267207+NikolaiAndrov@users.noreply.github.com>
2024-10-23 23:02:18 +0300

    Find two diagonal sums and return abs value

---
+++ MultidimensionalArraysLab/DiagonalSum/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace DiagonalSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] nums = new int[3, 3]
+            {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 9, 8, 9 }
+            };
+
+            Console.WriteLine(DiagonalDifference(nums));
+        }
+
+        public static int DiagonalDifference(int[,] nums)
+        {
+            int leftDiagonalSum = 0;
+            int rightDiagonalSum = 0;
+
+            for (int row = 0; row < nums.GetLength(0); row++)
+            {
+                leftDiagonalSum += nums[row, row];
+                rightDiagonalSum += nums[row, nums.GetLength(0) - 1 - row];
+            }
+
+            int absValue = Math.Abs(leftDiagonalSum - rightDiagonalSum);
+            return absValue;
+        }
+
+    }
+}
